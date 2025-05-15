698349e6bb14c81053cc77afc4eb92040e976cf2
Enger Alexander Castillo <enyergroud123@gmail.com>
2025-05-15 16:18:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function validateCedula() {
+    const cedula = document.getElementById('cedula').value;
+    const resultDiv = document.getElementById('result');
+
+    if (!cedula) {
+        resultDiv.textContent = "Por favor, ingrese una cédula.";
+        return;
+    }
+
+    // Cambia https:// a http://
+    fetch(`http://api-validar-cedula.onrender.com/api/validate?cedula=${cedula}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error en la red');
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.isValid) {
+                resultDiv.textContent = `La cédula ${data.cedula} es válida.`;
+            } else {
+                resultDiv.textContent = `La cédula ${data.cedula} no es válida.`;
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            resultDiv.textContent = "Ocurrió un error al validar la cédula.";
+        });
+}
